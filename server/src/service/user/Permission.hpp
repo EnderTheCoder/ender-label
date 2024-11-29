2a3fa22c9bfee4acb70537178041d604765118ca
EnderTheCoder <ggameinvader@gmail.com>
2024-11-29 11:29:00 +0000

    tree-like perm api

--- server/src/service/user/Permission.hpp
+++ server/src/service/user/Permission.hpp
@@ -12,12 +12,19 @@ namespace ender_label::service::user {
     constexpr char table_name_permission[] = "permission";
 
     class Permission final : public ServiceBean<table_name_permission, data::PermissionDto> {
+    public:
         auto parent() {
-            return getById(this->getDto()->parent);
+            return getById(getDto()->parent);
         }
 
         auto children() {
             return toWrappedList(getByField("parent", getId()));
+        }
+
+        auto get_is_child_of(auto perm_id) {
+        }
+
+        auto get_is_parent_of(auto perm_id) {
         }
     };
 }
