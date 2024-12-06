3dcf5b657d88ac50fd60205d418a050f6192d49a
EnderTheCoder <ggameinvader@gmail.com>
2024-12-06 10:32:01 +0000

    rename to token

--- server/src/util/AuthUtil.hpp
+++ server/src/util/AuthUtil.hpp
@@ -17,7 +17,7 @@
 namespace ender_label::util {
     std::shared_ptr<service::user::User> get_current_user(auto sess) {
         using namespace ender_label::service::user;
-        auto users = User::toWrappedList(User::getByField("session", sess));
+        auto users = User::toWrappedList(User::getByField("token", sess));
         if (users.empty()) {
             return nullptr;
         }
